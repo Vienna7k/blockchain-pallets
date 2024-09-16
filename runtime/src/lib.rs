983f01cd20b21bc8cc329d7543a8471b0868963c
mathamen <mathamen@protonmail.com>
2024-09-15 21:17:26 -0300

    fiz funcionar

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -249,6 +249,7 @@ impl pallet_sudo::Config for Runtime {
 
 /// Configure the pallet-template in pallets/template.
 impl pallet_template::Config for Runtime {
+	type MaxLength =  ConstU32<200>;
 	type RuntimeEvent = RuntimeEvent;
 	type WeightInfo = pallet_template::weights::SubstrateWeight<Runtime>;
 }
